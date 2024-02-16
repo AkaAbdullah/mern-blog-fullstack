4dd1f0822abef6dc224543b5319dcac477e1473f
Abdullah Hussain <akaabdullah00@gmail.com>
2024-02-17 00:12:25 +0500

    server added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import { SignUp } from "./pages/SignUp";
 import { Dashboard } from "./pages/Dashboard";
 import { Projects } from "./pages/Projects";
 import { About } from "./pages/About";
+import { Header } from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>this is home page</h1>
+    </>
+  );
 };
